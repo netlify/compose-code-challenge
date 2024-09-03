b416e75bd9e3c4547aca32dab9865daee22fa8ee
Phil Hawksworth <phil@netlify.com>
2024-09-03 15:03:35 +0100

    URL updates

--- netlify/edge-functions/submit.ts
+++ netlify/edge-functions/submit.ts
@@ -1,12 +1,15 @@
 import { Config, Context } from "@netlify/edge-functions";
 
-
+/*
+  This edge function quietly submits the details of this site to us
+  so that we can add it to our leaderboard and enter it into the 
+  prize draw at Netlify Compose Conf 2024
+*/
 export default async (request: Request, context: Context) => {
 
   const site = context.site;
   site['context'] = context.deploy.context;  
   await fetch('https://compose-code-challenge.netlify.app/submissions', {
-  // await fetch('http://localhost:8000/submissions', {
     method: 'POST',
     headers: {
       "Content-Type": "application/json",
